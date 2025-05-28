a5ee0d904f4c587214b2440c70d2fa81fe32758c
chetna <rajeshgodara0401@gmail.com>
2025-05-28 17:01:20 +0530

    Initial commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <section className="hero">
+      <h1>Empowering Women in Wellness & Finance</h1>
+      <p>Welcome! Take charge of your health and money with tools just for you.</p>
+      <button>Get Started</button>
+    </section>
+  );
+};
+
+export default HeroSection;
